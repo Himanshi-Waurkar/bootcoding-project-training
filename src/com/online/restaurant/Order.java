382564bcf907e6ab5f6e9026710817c8d0f0aa40
Himanshi <himanshiwaurkar2003@gmail.com>
2022-10-12 16:20:54 +0530

    first commit

---
+++ src/com/online/restaurant/Order.java
@@ -0,0 +1,81 @@
+package com.online.restaurant;
+
+import java.util.List;
+import  java.util.Date;
+
+public class Order {
+    private long orderId; //orderNumber
+    private double totalAmount;
+    private Vendor vendor;// veeeeeendor detail
+    private Customer customer;// Customer detail
+    private String deliveryAddress;
+
+    public long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(long orderId) {
+        this.orderId = orderId;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public Vendor getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(Vendor vendor) {
+        this.vendor = vendor;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public List<OrderMenuItem> getMenuItem() {
+        return menuItem;
+    }
+
+    public void setMenuItem(List<OrderMenuItem> menuItem) {
+        this.menuItem = menuItem;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    private List<OrderMenuItem> menuItem;
+    private Date orderDate;
+    private String orderStatus;
+
+}
---
+++ src/com/online/restaurant/app/Application.java
@@ -0,0 +1,64 @@
+package com.online.restaurant.app;
+
+import com.online.restaurant.Customer;
+import com.online.restaurant.Order;
+import com.online.restaurant.Vendor;
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.Date;
+
+public class Application {
+    public static void main(String[] args) {
+        Customer himanshi=new Customer();
+
+        himanshi.setName("Himanshi");
+        himanshi.setCity("Nagpur");
+        himanshi.setAddress("New Nandanvan,Nagpur");
+        himanshi.setState("Maharashtra");
+        himanshi.setEmailId("abc@gmail.com");
+        himanshi.setPhoneNumber("6483493758L");
+
+
+        System.out.println("Name :"+ himanshi.getName());
+        System.out.println("City :"+ himanshi.getCity());
+        System.out.println("Address :"+ himanshi.getAddress());
+        System.out.println("State :"+ himanshi.getState());
+        System.out.println("Email Id:"+ himanshi.getEmailId());
+        System.out.println("Phone:"+ himanshi.getPhoneNumber());
+
+
+
+
+        Vendor haldiram = new Vendor();
+        haldiram.setName("Haldiram Veg Restaurant");
+        haldiram.setAddress("Ajni Square Nagpur");
+        haldiram.setCategory("VEG");
+        haldiram.setRating(5.0);
+        haldiram.setState("Maharashtra");
+        haldiram.setCity("Nagpur");
+
+
+        System.out.println("Vendor Details:");
+        System.out.println("Name: "+ haldiram.getName());
+        System.out.println("Address: "+ haldiram.getAddress());
+        System.out.println("Category: "+ haldiram.getCategory());
+        System.out.println("Phone: "+ haldiram.getPhoneNumber());
+        System.out.println("Rating: "+ haldiram.getRating());
+        System.out.println("State: "+ haldiram.getState());
+        System.out.println("City: "+ haldiram.getCity());
+
+
+        Order order = new Order();
+        order .setCustomer(himanshi);
+        order .setVendor(haldiram);
+        order .setOrderDate(new Date());
+        order .setDeliveryAddress("101,civil line nagpur");
+
+        System.out.println("Order Details:");
+        System.out.println("Odder Date: "+ order.getOrderDate());
+        System.out.println("Order Delivery Address: "+ order.getDeliveryAddress());
+
+
+    }
+
+}
